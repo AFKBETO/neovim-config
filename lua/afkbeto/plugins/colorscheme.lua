e708024d7636fc704b75d31b52cb2aefe96acd66
afkbeto <qv.nguyen00@gmail.com>
2024-03-25 21:53:13 +0100

    add: change default line number color

--- lua/afkbeto/plugins/colorscheme.lua
+++ lua/afkbeto/plugins/colorscheme.lua
@@ -4,7 +4,15 @@ return {
 		lazy = false,
 		priority = 1000,
 		config = function()
-			require 'nordic' .load()
+			require('nordic').setup({
+				bold_keyword = true,
+				override = {
+					LineNr = {
+                        fg = "#d99cd0",
+                    },
+				}
+			})
+			vim.cmd([[colorscheme nordic]])
 		end
 	},
 	-- {
