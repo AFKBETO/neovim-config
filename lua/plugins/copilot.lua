93660a71a05ec6447206f6dbc5ef5066cf54a94b
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-05-31 14:05:51 +0200

    feat: add toggle copilot keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,20 @@
 return {
-  "github/copilot.vim"
+	"github/copilot.vim",
+	config = function()
+		local keymap = vim.keymap
+		vim.g.copilot_enabled = true
+
+		local toggle_copilot = function()
+			if vim.g.copilot_enabled then
+				vim.g.copilot_enabled = false
+				print("Copilot disabled")
+			else
+				vim.g.copilot_enabled = true
+				print("Copilot enabled")
+			end
+		end
+		keymap.set("n", "<leader>ct", toggle_copilot, {
+			desc = "Toggle Copilot",
+		})
+	end,
 }
