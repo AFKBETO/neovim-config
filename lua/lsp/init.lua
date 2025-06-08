2cf0edcef03cad4b3530c8421b50085294f71a61
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-06-08 21:15:01 +0200

    refactor: simplify lsp mapping

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -54,54 +54,44 @@ end, { desc = 'Toggle diagnostic virtual_lines' })
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('my.lsp', {}),
-	callback = function(args)
-		local opts = { noremap = true, silent = true, buffer = args.buf }
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		local keymap = vim.keymap
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			if mode == nil then
+				mode = "n"
+			end
+			vim.keymap.set(mode, keys, func, { noremap = true, silent = true, buffer = event.buf, desc = desc })
+		end
+		local client = assert(vim.lsp.get_client_by_id(event.data.client_id))
 
-		opts.desc = "Restart LSP"
-		keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary	
+		map("<leader>lr", "<cmd>LspRestart<CR>", "Restart LSP")
+		map("<leader>li", "<cmd>LspInfo<CR>", "Show LSP info")
 
 		if client:supports_method('textDocument/implementation') then
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+			map("gi", "<cmd>Telescope lsp_implementations<CR>", "Show LSP implementations") -- show lsp implementations
 		end
 		if client:supports_method('textDocument/typeDefinition') then
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+			map("gt", "<cmd>Telescope lsp_type_definitions<CR>", "Show LSP type definitions") -- show lsp type definitions
 		end
 		if client:supports_method('textDocument/references') then
-			opts.desc = "Show LSP references"
-			keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+			map("gr", "<cmd>Telescope lsp_references<CR>", "Show LSP references") -- show lsp references
 		end
 		if client:supports_method('textDocument/declaration') then
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+			map("gD", vim.lsp.buf.declaration, "Go to declaration", "n") -- go to declaration
 		end
 		if client:supports_method('textDocument/defintion') then
-			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+			map("gd", "<cmd>Telescope lsp_definitions<CR>", "Show LSP definitions") -- show lsp definitions
 		end
 		if client:supports_method('textDocument/codeAction') then
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			map("<leader>ca", vim.lsp.buf.code_action, "See available code action", { "n", "v" }) -- see available code actions, in visual mode will apply to selection
 		end
 		if client:supports_method('textDocument/rename') then
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+			map("<leader>rn", vim.lsp.buf.rename, "Smart rename")
 		end
 		if client:supports_method('textDocument/diagnostic') then
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.get_prev, opts) -- jump to previous diagnostic in buffer
-
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.get_next, opts) -- jump to next diagnostic in buffer
+			map("<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Show buffer diagnostics") -- show diagnostics for file
+			map("<leader>d", vim.diagnostic.open_float, "Show line diagnostics")         -- show diagnostics for line
+			map("[d", vim.diagnostic.get_prev, "Go to previous diagnostic")              -- jump to previous diagnostic in buffer
+			map("]d", vim.diagnostic.get_next, "Go to next diagnostic")                  -- jump to next diagnostic in buffer
 		end
 
 		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
@@ -109,21 +99,21 @@ vim.api.nvim_create_autocmd('LspAttach', {
 			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
 			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
 			-- client.server_capabilities.completionProvider.triggerCharacters = chars
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
+			vim.lsp.completion.enable(true, client.id, event.buf, { autotrigger = false })
 		end
 		-- Auto-format ("lint") on save.
 		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
 		if not client:supports_method('textDocument/willSaveWaitUntil') and client:supports_method('textDocument/formatting') then
 			vim.api.nvim_create_autocmd('BufWritePre', {
 				group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-				buffer = args.buf,
+				buffer = event.buf,
 				callback = function()
-					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+					vim.lsp.buf.format({ bufnr = event.buf, id = client.id, timeout_ms = 1000 })
 				end,
 			})
 		end
 		if client.name == "denols" or client.name == "ts_ls" then
-			require("twoslash-queries").setup(client, args.buf)
+			require("twoslash-queries").setup(client, event.buf)
 		end
 	end,
 })
