e9a7fcbd77c8e90665733d7679ed29ea276c30e1
QV Nguyen <91276801+AFKBETO@users.noreply.github.com>
2025-07-06 01:46:37 +0200

    feat: update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,13 @@
 return {
-	{
-		'metalelf0/black-metal-theme-neovim',
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require('black-metal').setup({
-				theme = 'bathory',
-				variant = 'light',
-				alt_bg = true,
-			})
-			require('black-metal').load()
-		end,
-	},
+	'projekt0n/github-nvim-theme',
+	name = 'github-theme',
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require('github-theme').setup({
+		})
+
+		vim.cmd('colorscheme github_dark')
+	end,
+
 }
